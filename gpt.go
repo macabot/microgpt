d790fb57acfda8b4b2e518e922e9348ea7879b71
macabot <1422579+macabot@users.noreply.github.com>
2026-09-15 16:26:48 +0200

    Create state

--- gpt.go
+++ gpt.go
@@ -1,8 +1,10 @@
 package microgpt
 
+import "math/rand"
+
 func sum(values []*Value) *Value {
 	if len(values) == 0 {
-		return nil
+		return NewValue(0, nil, nil)
 	}
 	s := values[0]
 	for i := 1; i < len(values); i++ {
@@ -63,3 +65,87 @@ func RMSNorm(x []*Value) []*Value {
 	}
 	return norm
 }
+
+type Layer struct {
+	// AttnWq Self-Attention Query Weights
+	AttnWq [][]*Value
+	// AttnWk Self-Attention Key Weights
+	AttnWk [][]*Value
+	// AttnWv Self-Attention Value Weights
+	AttnWv [][]*Value
+	// AttnWo Self-Attention Output Weights
+	AttnWo [][]*Value
+	// MLPFC1 First Fully Connected layer of the Multilayer Perceptron
+	MLPFC1 [][]*Value
+	// MLPFC2 Second Fully Connected layer of the Multilayer Perceptron
+	MLPFC2 [][]*Value
+}
+
+type State struct {
+	HeadDim int
+
+	// WTE Weight Token Embedding
+	WTE [][]*Value
+	// WPE Weight Position Embedding
+	WPE [][]*Value
+	// LMHead Language Modeling Head
+	LMHead [][]*Value
+	Layers []*Layer
+}
+
+func gauss(mean, std float32) *Value {
+	data := mean + float32(rand.NormFloat64())*std
+	return NewValue(data, nil, nil)
+}
+
+func matrix(nOut, nIn int, std float32) [][]*Value {
+	m := make([][]*Value, nOut)
+	for r := range nOut {
+		row := make([]*Value, nIn)
+		for i := range nIn {
+			row[i] = gauss(0, std)
+		}
+		m[r] = row
+	}
+	return m
+}
+
+func NewState(vocabSize, layerSize int, embedDim int, blockSize int, heads int, std float32) *State {
+	headDim := embedDim / heads
+	layers := make([]*Layer, layerSize)
+	for i := range layerSize {
+		layers[i] = &Layer{
+			AttnWq: matrix(embedDim, embedDim, std),
+			AttnWk: matrix(embedDim, embedDim, std),
+			AttnWv: matrix(embedDim, embedDim, std),
+			AttnWo: matrix(embedDim, embedDim, std),
+			MLPFC1: matrix(4*embedDim, embedDim, std),
+			MLPFC2: matrix(embedDim, 4*embedDim, std),
+		}
+	}
+	return &State{
+		HeadDim: headDim,
+		WTE:     matrix(vocabSize, embedDim, std),
+		WPE:     matrix(blockSize, embedDim, std),
+		LMHead:  matrix(vocabSize, embedDim, std),
+	}
+}
+
+// func GPT(state *State, tokenID int, posID int, keys []*Value, values []*Value) []*Value {
+// 	tokenEmb := state.WTE[tokenID]
+// 	posEmb := state.WPE[posID]
+// 	x := make([]*Value, len(tokenEmb))
+// 	for i := range tokenEmb {
+// 		x[i] = tokenEmb[i].Add(posEmb[i])
+// 	}
+// 	x = RMSNorm(x)
+// 	for _, layer := range state.Layers {
+// 		xResidual := x
+// 		x = RMSNorm(x)
+// 		q := Linear(x, layer.AttnWq)
+// 		k := Linear(x, layer.AttnWk)
+// 		v := Linear(x, layer.AttnWv)
+
+// 	}
+
+// }
